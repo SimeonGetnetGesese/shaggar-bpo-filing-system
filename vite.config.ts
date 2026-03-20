f0a12a864cdadc083e748d00b5d4cf45d8842269
Simeon Getnet Gesese <84731634+SimeonGetnetGesese@users.noreply.github.com>
2026-03-21 01:04:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/shaggar-bpo-filing-system/', // REPLACE THIS with your exact repo name on GitHub
+})
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
